1e4f7783c53a123e2802790b06422707af894472
Hugo <harturo_dhd@hotmail.com>
2024-11-29 00:11:53 -0600

    delete one duplicate provider to fix error

--- iac/provider.tf
+++ iac/provider.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 provider "kubernetes" {
   host                   = aws_eks_cluster.eks.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
