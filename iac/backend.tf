be0d02e1a08d290a2327351b93a37a607cd683ff
Hugo <harturo_dhd@hotmail.com>
2024-11-29 00:30:49 -0600

    backend change bucket name

--- iac/backend.tf
+++ iac/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state"
+    bucket         = "tfstate_eks"
     key            = "eks-cluster/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
