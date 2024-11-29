2cec7e87152df77ff6e14c97324d1d0583c686dc
Hugo <harturo_dhd@hotmail.com>
2024-11-29 00:19:33 -0600

    remove terraform.yml not used

--- iac/main.tf
+++ iac/main.tf
@@ -17,17 +17,3 @@ resource "aws_instance" "my_instance" {
     Name = "MyInstance"
   }
 }
-
-# Crear una base de datos RDS
-resource "aws_db_instance" "my_db" {
-  allocated_storage    = 20
-  db_instance_class    = "db.t2.micro"
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_identifier  = "mydbinstance"
-  name                 = "mydb"
-  username             = "admin"
-  password             = "password"
-  parameter_group_name = "default.mysql5.7"
-  skip_final_snapshot  = true
-}
